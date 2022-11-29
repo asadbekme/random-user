07ba2e2ff9bcea162c465ae958f2ab2cb67a074b
Асадбек* <asadbekme2002@gmail.com>
2022-11-29 11:42:51 +0500

    operation is completed!

--- js/request.js
+++ js/request.js
@@ -1,5 +1,50 @@
-// api
-const API = 'https://randomuser.me/api/?results=9'
+// API
+const API_URL = 'https://randomuser.me/api/?results=9'
 
-// for leader
+// for loader
 const overlay = document.getElementById('overlay')
+
+const loaderToggle = (toggle) => {
+    if (toggle) {
+        overlay.classList.remove('hidden')
+    } else {
+        overlay.classList.add('hidden')
+    }
+}
+
+// get data
+const getData = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+
+        request.addEventListener('readystatechange', () => {
+            // console.log(request)
+            if (request.readyState < 4) {
+                // console.log('Loading...')
+                loaderToggle(true)
+            } else if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText)
+                resolve(data.results)
+                loaderToggle(false) 
+            } else if (request.readyState === 4) {
+                reject('Error!!!')
+                loaderToggle(false)
+            }
+        })
+
+        request.open('GET', resource)
+        request.send()
+    })
+}
+
+// load
+const reload = () => {
+    getData(API_URL)
+        .then((data) => {
+            console.log('DATA', data)
+        }).catch((err) => {
+            console.log(err)
+        })
+}
+
+document.addEventListener('DOMContentLoaded', reload)
